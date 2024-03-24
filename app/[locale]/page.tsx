f1956246386ae34e20fe5819377c08dbc0f5c15a
Pawel <pawel.bornikowski@gmail.com>
2024-03-24 15:38:48 +0100

    feat: add animations to landpage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,15 +7,85 @@ import { SiTailwindcss } from "react-icons/si";
 import { motion } from "framer-motion";
 export default function Home() {
   return (
-    <main className="w-full max-w-7xl  h-screen mx-auto pt-24 text-white relative">
+    <main className="w-full max-w-7xl  h-screen mx-auto pt-24 text-white relative overflow-hidden">
       <div className="flex flex-col w-full max-w-2xl mx-auto  gap-4">
-        <p className="text-center font-thin">Hey! I am</p>
-        <h1 className="text-7xl text-center font-thin">Paweł</h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+            transform: "translateY(20px)",
+          }}
+          animate={{
+            opacity: 1,
+            transform: "translateY(0)",
+          }}
+          className="text-center text-lg font-thin"
+        >
+          Hey! I am
+        </motion.p>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            transform: "translateY(20px)",
+          }}
+          animate={{
+            opacity: 1,
+            transform: "translateY(0)",
+          }}
+          transition={{
+            delay: 0.2,
+          }}
+          className="text-7xl text-center font-thin"
+        >
+          Paweł
+        </motion.h1>
         <h1 className="text-6xl text-center mt-12 flex flex-col sm:flex-row font-bold justify-between gap-4 px-4 sm:px-0">
-          <span className="w-full flex sm:items-end sm:justify-end">Front-end</span>{" "}
-          <span className="w-full flex items-end justify-end sm:items-start sm:justify-start">Developer</span>
+          <motion.span
+            initial={{
+              opacity: 0,
+              transform: "translateX(-60px)",
+            }}
+            animate={{
+              opacity: 1,
+              transform: "translateY(0)",
+            }}
+            transition={{
+              delay: 0.4,
+            }}
+            className="w-full flex sm:items-end sm:justify-end"
+          >
+            Front-end
+          </motion.span>{" "}
+          <motion.span
+            initial={{
+              opacity: 0,
+              transform: "translateX(60px)",
+            }}
+            animate={{
+              opacity: 1,
+              transform: "translateY(0)",
+            }}
+            transition={{
+              delay: 0.4,
+            }}
+            className="w-full flex items-end justify-end sm:items-start sm:justify-start"
+          >
+            Developer
+          </motion.span>
         </h1>
-        <div className="flex justify-around mt-16 text-6xl">
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.5
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1
+          }}
+          transition={{
+            delay: 0.6,
+          }}
+          className="flex justify-around mt-16 text-6xl"
+        >
           <motion.div
             initial={{
               transform: "translateY(-20px)",
@@ -80,10 +150,23 @@ export default function Home() {
           >
             <SiTailwindcss />
           </motion.div>
-        </div>
+        </motion.div>
       </div>
 
-      <div className="h-[300px] w-[1px] absolute top-[450px] sm:top-60 left-[50%] -translate-x-1/2 bg-gradient-to-b from-purple-500 to-indigo-500"></div>
+      <motion.div
+        initial={{
+         
+          scale: 0
+        }}
+        animate={{
+         
+          scale: 1
+        }}
+        transition={{
+          delay: 0.6,
+        }}
+        className="h-[150px] sm:h-[300px] w-[1px] absolute top-[450px] sm:top-60 left-[50%] -translate-x-1/2 bg-gradient-to-b from-purple-500 to-indigo-500"
+      ></motion.div>
     </main>
   );
 }
