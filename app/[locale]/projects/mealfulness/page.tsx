15e7ae1a084278b9098219ca2c8348bd083fd802
Pawel <pawel.bornikowski@gmail.com>
2024-03-26 22:43:43 +0100

    styles: fix margins

--- app/[locale]/projects/mealfulness/page.tsx
+++ app/[locale]/projects/mealfulness/page.tsx
@@ -20,9 +20,9 @@ const page = () => {
   const splitStackText = stackText.split("");
   return (
     <main className="w-full  h-screen  text-white flex-col ">
-      <div className="w-full max-w-7xl mx-auto mt-44 sm:mt-32 flex gap-20 flex-col sm:flex-row px-4">
-        <div className="sm:flex-1 flex flex-col gap-8 justify-center items-start">
-          <h3 className="text-6xl sm:text-8xl">Mealfulness</h3>
+      <div className="w-full max-w-7xl mx-auto mt-44 sm:mt-64 lg:mt-32 flex gap-20 flex-col lg:flex-row px-4">
+        <div className="lg:flex-1 flex flex-col gap-8 justify-center items-start">
+          <h3 className="text-6xl lg:text-8xl">Mealfulness</h3>
           <span className="h-[1px] w-full opacity-50 bg-white"></span>
           <div className="flex w-full justify-between">
             <ul className="flex gap-4">
@@ -34,7 +34,7 @@ const page = () => {
             </ul>
             <span>2024</span>
           </div>
-          <p className="text-md">
+          <p className="text-lg">
             Dynamic meal tracking platform seamlessly integrated with nutrition
             data from external APIs. Enhanced with AI capabilities for
             personalized meal guidance, the platform facilitates effortless
@@ -47,7 +47,7 @@ const page = () => {
             <li>Live</li>
           </ul>
         </div>
-        <div className=" h-[700px] sm:flex-1 relative">
+        <div className=" h-[700px] lg:flex-1 relative">
           <Image
             fill
             src={chatMock}
@@ -57,7 +57,7 @@ const page = () => {
         </div>
       </div>
 
-      <div className="w-full h-[50%] sm:h-full relative my-32 sm:mt-32 flex justify-start items-start bg-gray-800   ">
+      <div className="w-full h-[50%] lg:h-full relative my-32 lg:mt-32 flex justify-start items-start bg-gray-800   ">
         <Image
           fill
           src={dashboardMock}
@@ -71,26 +71,26 @@ const page = () => {
           <Section
             title={section.title}
             text={section.text}
-            className={index % 2 === 0 ? "sm:order-1" : "sm:order-2"}
+            className={index % 2 === 0 ? "lg:order-1" : "lg:order-2"}
           />
           <ImageSection
             src={section.image}
             alt={section.imageAlt}
             width={section.imageWidth}
             height={section.imageHeight}
-            className={index % 2 === 0 ? "sm:order-2" : "sm:order-1"}
+            className={index % 2 === 0 ? "lg:order-2" : "lg:order-1"}
           />
         </SectionWrapper>
       ))}
 
-      <div className="w-full min-h-[60%] max-w-7xl mx-auto relative  flex gap-12 mt-32 flex-col sm:flex-row px-4 pb-32  ">
+      <div className="w-full min-h-[60%] max-w-7xl mx-auto relative  flex gap-12 mt-32 sm:mt-64 lg:mt-32 flex-col lg:flex-row px-4 pb-32  ">
         <div className="flex justify-start items-start flex-1 gap-12">
           <h4 className="text-4xl uppercase flex flex-col gap-8 stackText relative">
             {splitStackText.map((item, index) => (
               <span key={index}>{item}</span>
             ))}
           </h4>
-          <div className="text-5xl grid grid-cols-2 gap-y-10 gap-x-14 w-full sm:w-1/4 place-items-center sm:place-items-start">
+          <div className="text-5xl grid grid-cols-2 gap-y-10 gap-x-14 w-full lg:w-1/4 place-items-center lg:place-items-start">
             <BiLogoTypescript />
             <SiNextdotjs />
             <SiTailwindcss />
@@ -101,9 +101,9 @@ const page = () => {
             <SiJest />
           </div>
         </div>
-        <div className="sm:flex-1 mt-32">
+        <div className="lg:flex-1 mt-32">
           <h4 className="text-3xl">Other technologies I used</h4>
-          <div className=" mt-6 gap-4 flex flex-wrap justify-start items-center w-full sm:w-3/4">
+          <div className=" mt-6 gap-4 flex flex-wrap justify-start items-center w-full lg:w-3/4">
             {technologies.map((tech) => (
               <TechnologyItem key={tech} name={tech} />
             ))}
@@ -152,14 +152,14 @@ const sections = [
 ];
 const TechnologyItem = ({ name }: { name: string }) => (
   <div className="flex justify-center items-center">
-    <span className="bg-gradient-to-r from-purple-500 to-indigo-600 rounded-md p-2 font-medium">
+    <span className="bg-gradient-to-r from-purple-500 to-indigo-600 rounded-lg p-2 font-medium">
       {name}
     </span>
   </div>
 );
 
 const SectionWrapper = ({ children }: { children: React.ReactNode }) => (
-  <div className="w-full h-[70%] max-w-7xl mx-auto relative  flex gap-4 sm:gap-12 flex-col sm:flex-row  mt-44 sm:mt-0">
+  <div className="w-full h-[70%] max-w-7xl mx-auto relative  flex gap-4 lg:gap-12 flex-col lg:flex-row  mt-44 lg:mt-0">
     {children}
   </div>
 );
@@ -174,13 +174,13 @@ const Section = ({
 }) => (
   <div
     className={cn(
-      "sm:flex-1 flex flex-col gap-8 justify-center items-start px-4",
+      "lg:flex-1 flex flex-col gap-8 justify-center items-start px-4",
       className
     )}
   >
     <h4 className="text-4xl">{title}</h4>
     <span className="h-[1px] w-full opacity-50 bg-white"></span>
-    <p className="text-md">{text}</p>
+    <p className="text-lg">{text}</p>
   </div>
 );
 
@@ -199,11 +199,11 @@ const ImageSection = ({
 }) => (
   <div
     className={cn(
-      "sm:flex-1 px-6 lg:px-8 flex justify-center items-center ",
+      "lg:flex-1 px-6 lg:px-8 flex justify-center items-center ",
       className
     )}
   >
-    <div className="mt-16 flow-root sm:mt-24">
+    <div className="mt-16 flow-root lg:mt-24">
       <div className="-m-2 rounded-xl bg-slate-50/5 p-2 ring-1 ring-inset ring-slate-50/10 lg:-m-4 lg:rounded-2xl lg:p-4">
         <Image
           src={src}
@@ -211,7 +211,7 @@ const ImageSection = ({
           height={height}
           alt={alt}
           quality={100}
-          className="rounded-md bg-white p-2 sm:p-8  shadow-2xl ring-1 ring-gray-900/10"
+          className="rounded-lg bg-white p-2 lg:p-8  shadow-2xl ring-1 ring-gray-900/10"
         />
       </div>
     </div>
