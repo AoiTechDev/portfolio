7b2681996a1779db14bd77e1c7a3ff7e38dc9d85
Pawel <pawel.bornikowski@gmail.com>
2024-04-03 17:57:42 +0200

    refactor: create dynamic project section component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,45 +1,21 @@
-'use client'
+"use client";
 import React from "react";
-import chatMock from "@/assets/chat-mobile.png";
+import landpageMock from "@/assets/landpage-mobile-portrait.png";
 import Image from "next/image";
 import Link from "next/link";
-import {motion} from "framer-motion";
+import ProjectSection from "./common/ProjectSection/ProjectSection";
+
 const links = [
   { label: "Github", href: "https://github.com/AoiTechDev" },
   { label: "Live", href: "https://mealfulness.vercel.app/" },
 ];
-const MotionComponent = ({
-  children,
-  delay,
-  x = -100,
-  y = 0,
-}: {
-  children: React.ReactNode;
-  delay: number;
-  x?: number;
-  y?: number;
-}) => (
-  <motion.div
-    initial={{
-      opacity: 0,
-      x,
-      y,
-    }}
-    animate={{
-      opacity: 1,
-      x: 0,
-      y: 0,
-    }}
-    transition={{ delay }}
-  >
-    {children}
-  </motion.div>
-);
+
 const Projects = () => {
   return (
     <div className="w-full max-w-7xl  h-screen mx-auto  sm:pt-24 text-white mt-80">
       <h2 className="text-4xl  text-center uppercase">selected Projects</h2>
-      <div className="w-full  mt-32 flex gap-20 flex-col lg:flex-row">
+
+      {/* <div className="w-full  mt-32 flex gap-20 flex-col lg:flex-row">
         <div className="lg:flex-1 flex flex-col gap-8 justify-center items-start px-4">
         <h3 className="text-6xl lg:text-8xl">Mealfulness</h3>
           <span className="h-[1px] w-full opacity-50 bg-white"></span>
@@ -73,10 +49,26 @@ const Projects = () => {
           </div>
         </div>
         <div className="bg- h-[700px] lg:flex-1 relative">
-          <Image fill src={chatMock} alt="mealfulness landing page" className="object-contain"/>
+          <Image fill src={landpageMock} alt="mealfulness landing page" className="object-contain"/>
         </div>
-      </div>
-      
+      </div> */}
+
+      <ProjectSection
+        title="Mealfulness"
+        stack={["Next.js", "Typescript", "Tailwind CSS"]}
+        year="2024"
+        description="Dynamic meal tracking platform integrated with external nutrition API and AI capabilities for personalized meal guidance."
+        links={links}
+        project="/projects/mealfulness"
+        image={
+          <Image
+            fill
+            src={landpageMock}
+            alt="mealfulness landing page"
+            className="object-contain"
+          />
+        }
+      />
     </div>
   );
 };
