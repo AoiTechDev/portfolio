8a8b5f43a8dab54c923b84d86c4af9dfb2a1e38b
Pawel <pawel.bornikowski@gmail.com>
2024-04-11 21:52:54 +0200

    style: change gradient in header

--- components/About.tsx
+++ components/About.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <section className="w-full max-w-7xl  h-screen mx-auto  sm:pt-24 text-white mt-32 relative">
       {/* <div className="bg-gradient-to-r from-purple-500 to-indigo-600 w-full h-32 blur-md"></div> */}
       <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0  -z-10 overflow-hidden blur-2xl left-52 "
-        >
-         <div  className="bg-gradient-to-r from-purple-500 to-indigo-600 w-[250px] h-[600px]"/>
+        aria-hidden="true"
+        className="pointer-events-none absolute inset-x-0  -z-10 overflow-hidden blur-3xl left-20 top-[150px]"
+      >
+        <div className="bg-gradient-to-r from-purple-500 to-indigo-600 w-[600px] h-[250px] flex justify-center items-center">
+        
         </div>
-       
+      </div>
+
       <h2 className="text-[140px]  font-bold aboutHeader relative">About</h2>
-
+      <div className="w-full flex">
+        <div className="flex-1"></div>
+        <div className="flex-1">
+          <p></p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
