32e2b2cefecbd7521f09fd3dd75252e4548c0b2c
Pawel <pawel.bornikowski@gmail.com>
2024-05-07 14:08:05 +0200

    feat: add inputs to send email

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,18 +2,14 @@
 import React from "react";
 
 import { FaLinkedin } from "react-icons/fa";
-
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const Contact = () => {
   return (
-    <div className="w-full max-w-[1800px] min-h-screen flex  flex-col min-[1400px]:flex-row justify-center gap-12 min-[1400px]:justify-center items-center text-white my-32">
-      <div className=" order-2 min-[1400px]:order-1 min-[1400px]:flex-1 text-white flex justify-center items-center relative w-full">
-       
-        <div className="absolute flex flex-col gap-6 lg:w-[300px] bg-white">
-         
-        </div>
-      </div>
-      <div className="order-1 min-[1400px]:order-2 min-[1400px]:flex-1 flex-col min-[1400px]:h-screen  flex items-center  justify-around ">
+    <div className="w-full h-screen min-h-screen flex  flex-col min-[1400px]:flex-row justify-center gap-12 min-[1400px]:justify-center items-center text-white my-32">
+      <div className=" min-[1400px]:flex-1 flex-col min-[1400px]:h-screen  flex items-center  justify-around ">
         <div className="">
           <h2 className="text-8xl">Contact</h2>
           <div className="flex gap-2 justify-center items-center">
@@ -25,6 +21,26 @@ const Contact = () => {
           <h3 className="text-2xl">You can also find me here.</h3>
 
           <FaLinkedin className="text-5xl" />
+        </div>
+      </div>
+      <div className=" min-[1400px]:flex-1 text-white flex justify-center items-center  h-screen w-full">
+        <div className="w-1/2 space-y-6">
+          <div>
+            <Label>Your Name</Label>
+            <Input />
+          </div>
+          <div>
+            <Label>Your Email</Label>
+            <Input />
+          </div>
+          <div>
+            <Label>Subject</Label>
+            <Input />
+          </div>
+          <div>
+            <Label>Leave me a message</Label>
+            <Textarea />
+          </div>
         </div>
       </div>
     </div>
