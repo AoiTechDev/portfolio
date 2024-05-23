4b478adbd487b2a14d3969061950bd3497144fd6
Pawel <pawel.bornikowski@gmail.com>
2024-05-23 22:16:28 +0200

    styles: improve padding and shadows

--- components/About/Bio.tsx
+++ components/About/Bio.tsx
@@ -18,9 +18,9 @@ const Bio = () => {
   return (
     <div className="w-full flex flex-col lg:flex-row justify-center gap-10 items-center">
       <div className="order-2 lg:order-1  flex-1 gap-10 flex h-full flex-col">
-        <WobbleCard className=" h-full min-h-[180px] sm:min-h-[250px] bg-grid-white/[0.05]  flex justify-center items-center     rounded-2xl">
-          <RadialBlur />
-          <MotionComponent>
+        <WobbleCard className=" h-full min-h-[180px] sm:min-h-[240px] bg-grid-white/[0.05]  flex justify-start items-center     rounded-2xl">
+          {/* <RadialBlur /> */}
+          <MotionComponent className="pl-4">
             <p className="text-2xl sm:text-3xl font-semibold text-balance text-start">
               {locale === "en" ? (
                 <>
@@ -37,10 +37,10 @@ const Bio = () => {
             </p>
           </MotionComponent>
         </WobbleCard>
-        <WobbleCard className=" h-full min-h-[180px] sm:min-h-[250px] rounded-2xl relative bg-grid-white/[0.05]">
-          <RadialBlur />
-          <MotionComponent>
-            <p className="uppercase font-mono">{t("about:scoop")}</p>
+        <WobbleCard className=" h-full min-h-[180px] sm:min-h-[240px] rounded-2xl  relative bg-grid-white/[0.05]">
+          {/* <RadialBlur /> */}
+          <MotionComponent className="space-y-2 pl-4">
+            <p className="opacity-70 uppercase font-mono">{t("about:scoop")}</p>
             <p className="text-2xl sm:text-3xl  font-semibold  text-start bottom-4 left-4 text-balance">
               {t("about:matchmaking")}{" "}
               <GradientText text={t("about:hackathon")} />
@@ -49,7 +49,7 @@ const Bio = () => {
         </WobbleCard>
       </div>
       <div className="order-1 rounded-2xl overflow-hidden relative lg:order-2   flex justify-center items-center">
-        <RadialBlur />
+      
         <MotionComponent className="absolute bottom-4 sm:bottom-10 left-4 sm:left-6">
           <p className="text-2xl sm:text-3xl  font-semibold  text-start  text-balance">
             {t("about:priority1")} <GradientText text={t("about:colab")} />{" "}
